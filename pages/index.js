e5063d793d7c7d238e44061964648f53e2ade38c
terjeee <96795863+terjeee@users.noreply.github.com>
2022-11-28 03:00:17 +0100

    :)

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,36 @@
-export default function MeetUp() {
-  return <div>MeetUp</div>;
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "1",
+    title: "1",
+    address: "1",
+    image:
+      "https://www.snow-forecast.com/system/images/36932/original/Crystal-Mountain.jpg?1619614348",
+  },
+  {
+    id: "2",
+    title: "2",
+    address: "2",
+    image:
+      "https://www.snow-forecast.com/system/images/36932/original/Crystal-Mountain.jpg?1619614348",
+  },
+  {
+    id: "3",
+    title: "3",
+    address: "2",
+    image:
+      "https://www.snow-forecast.com/system/images/36932/original/Crystal-Mountain.jpg?1619614348",
+  },
+  {
+    id: "4",
+    title: "4",
+    address: "2",
+    image:
+      "https://www.snow-forecast.com/system/images/36932/original/Crystal-Mountain.jpg?1619614348",
+  },
+];
+
+export default function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
